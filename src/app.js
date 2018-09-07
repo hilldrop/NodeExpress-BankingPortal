5a91a22f2bd42c7d7a9346cb413743f543e3ae1b
hilldrop <braithwaitedemian@hotmail.com>
2018-09-07 11:50:21 +0100

    Test 4

--- src/app.js
+++ src/app.js
@@ -6,22 +6,14 @@ const express = require('express');
 
 const app = express();
 
+const {accounts, users, writeJSON } = require('./data');
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.urlencoded({extended: true}));
-
-const accountData = fs.readFileSync(
-    path.join(__dirname, 'json', 'accounts.json'), 'utf8'
-);
-const accounts = JSON.parse(accountData);
-
-const userData = fs.readFileSync(
-    path.join(__dirname, 'json', 'users.json'), 'utf8'
-);
-const users = JSON.parse(userData);
 
 
 app.get('/', (req, res) => {
@@ -47,9 +39,7 @@ app.get('/transfer', (req, res) => res.render('transfer'));
 app.post('/transfer', (req, res) => {
     accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
     accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
-    const accountsJSON = JSON.stringify(accounts, null, 4);
-    fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
-
+    writeJSON();
     res.render('transfer', { message: 'Transfer Completed' });
 });
 
@@ -61,8 +51,7 @@ app.get('/payment', (req, res) => res.render('payment', { account: accounts.cred
 app.post('/payment', (req, res) => { 
     accounts.credit.balance -= req.body.amount;
     accounts.credit.available += parseInt(req.body.amount, 10);
-    const accountsJSON = JSON.stringify(accounts, null, 4);
-    fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
+    writeJSON();
     res.render('payment', {message: 'Payment successfull', account:accounts.credit});
 
 });
@@ -72,7 +61,5 @@ app.post('/payment', (req, res) => {
 app.get('/profile', (req, res) => {
     res.render('profile', {user: users[0]});
 });
-
-
 
 app.listen(3000, ()=> console.log('Project is running on port 3000'));
